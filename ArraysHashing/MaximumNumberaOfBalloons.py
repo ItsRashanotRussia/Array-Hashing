20473611f8e094f3317948c43f6b6f96bdce8698
Rasha Boura <Rashaboura123@gmail.com>
2023-06-04 09:13:09 -0700

    MaximumNumberaOfBalloons.py

--- ArraysHashing/MaximumNumberaOfBalloons.py
+++ ArraysHashing/MaximumNumberaOfBalloons.py
@@ -1,3 +1,25 @@
+def max_number_of_balloons(text):
+    char_count = {}
+
+    #the freq of each char
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    #max num of instances of balloon
+    max_instances = float('inf')
+    for char in 'balon':
+        if char == 'l' or char == 'o':
+            count = char_count.get(char, 0) // 2
+        else:
+            count = char_count.get(char, 0)
+        max_instances = min(max_instances, count)
+
+    return max_instances
+
+
 """
 
 
