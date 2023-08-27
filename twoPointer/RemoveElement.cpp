6f19306bcd7a462c72a754798f1974b4d02fc19c
rasha <Rashaboura123@gmail.com>
2023-08-27 00:23:51 -0700

    Create RemoveElement.cpp

---
+++ twoPointer/RemoveElement.cpp
@@ -0,0 +1,22 @@
+// I don't think it's 100% correct but that's what I got
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) { //defining the removeElement function
+    int j = 0; //keeping track of the index for the modified array
+    
+  
+    for (int i = 0; i < nums.size(); i++) { //loop through each element in num
+        if (nums[i] != val) { //we have to check if it's equal to val
+            nums[j] = nums[i]; //if not assign the value of nums[i] to nums[j]
+            
+            j++; //increment to indicate the next poition
+        }
+    }
+    
+    //j == new length of the modified array
+    //new length == the count of elements that are not equal to val
+    return j;
+}
